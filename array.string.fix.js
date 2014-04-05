90770886f84fe65a19ac5945de2fb9910dcd3823
hosokawat <vwxyzzy@gmail.com>
2014-04-05 13:49:08 +0900

    init

---
+++ array.string.fix.js
@@ -0,0 +1,24 @@
+(function () {
+  Array.prototype.toString = function arrayString() {
+    function hasArray(array) {
+      for (var key in array) {
+        if (array[key] instanceof Array) return true;
+      }
+      return false;
+    }
+    var array = this;
+    if (!(array instanceof Array)) return this.toString();
+    if (hasArray(array) === false) {
+      return '[' + Array.prototype.join.call(array) + ']';
+    } else {
+      var ary = [];
+      for (var key in array) {
+        ary.push(
+          array[key] instanceof Array ? arrayString(array[key]) : array[key]);
+      }
+      return '[' + ary.join(',') + ']';
+    }
+  };
+
+})();
+
